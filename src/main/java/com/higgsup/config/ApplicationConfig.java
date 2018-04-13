eb64b4e830604bbfdc57dece75eeb711f810519e
Admin <anhvv@higgsup.com>
2018-04-13 14:17:21 +0700

    event - spring auto config

--- src/main/java/com/higgsup/config/ApplicationConfig.java
+++ src/main/java/com/higgsup/config/ApplicationConfig.java
@@ -1,12 +1,5 @@
 package com.higgsup.config;
 
-import com.higgsup.DemoApp;
-import com.higgsup.repository.StudentRepository;
-import com.higgsup.repository.impl.StudentRepositoryImpl;
-import com.higgsup.service.StudentService;
-import com.higgsup.service.impl.StudentServiceImpl;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
